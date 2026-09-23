6fe96eaaea1551a70315a9193d63ac8c2f497bd0
Kieu Tan Phat <kieuphat260@gmail.com>
2026-09-23 22:25:12 +0700

    Giới hạn bề mặt truy cập API cho ứng dụng Android

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,6 @@ import { Stream } from './stream';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.get(Stream).attach(app.getHttpServer());
   const host = process.env.HOST ?? '0.0.0.0';
   await app.listen(Number(process.env.PORT ?? 3000), host);
